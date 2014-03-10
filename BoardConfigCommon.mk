a8ba3acd3b8b8504e7824b5a7986f50ad027320d
Jake W <xboxlover360@gmail.com>
2014-03-09 20:03:31 -0500

    gee-common: EGL update

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,6 +138,18 @@ TARGET_USES_CM_POWERHAL := true
 USE_DEVICE_SPECIFIC_CAMERA:= true
 USE_DEVICE_SPECIFIC_QCOM_PROPRIETARY:= true
 
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
 HAVE_ADRENO_SOURCE:= false
